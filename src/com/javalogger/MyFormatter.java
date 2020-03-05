44bd62887b2c281c36d8234167b4dc110b8d9fc7
gurkay <g_basyigit@hotmail.com>
2020-03-06 00:43:51 +0300

    update

--- src/com/javalogger/MyFormatter.java
+++ src/com/javalogger/MyFormatter.java
@@ -5,13 +5,25 @@ import java.util.logging.Formatter;
 import java.util.logging.LogRecord;
 
 public class MyFormatter extends Formatter {
-	
+
 	@Override
 	public String format(LogRecord logRecord) {
-		String str = logRecord.getThreadID() + "::" + logRecord.getSourceClassName() + "::" 
-				+ logRecord.getSourceMethodName() + "::" + new Date(logRecord.getMillis()) + "::"
-				+ logRecord.getMessage() + "\n";
+		/*
+		 * String str = "<record>" + "<date>" + new Date() + "</date>" + "<millis>" +
+		 * new Date(logRecord.getMillis()) + "</millis>" + "<sequence>" + 996 +
+		 * "</sequence>" + "<logger> com.javalogger.TestCase </logger>" + "<class>" +
+		 * logRecord.getSourceClassName() + "</class>" + "<method>" +
+		 * logRecord.getSourceMethodName() + "</method>" + "<thread>" +
+		 * logRecord.getThreadID() + "</thread>" + "<message>" + logRecord.getMessage()
+		 * + "</message>" + "</record>";
+		 */
+
 		
+		  String str = logRecord.getThreadID() + "::" + logRecord.getSourceClassName()
+		  + "::" + logRecord.getSourceMethodName() + "::" + new
+		  Date(logRecord.getMillis()) + "::" + logRecord.getMessage() + "\n";
+		 
+
 		return str;
 	}
 }
