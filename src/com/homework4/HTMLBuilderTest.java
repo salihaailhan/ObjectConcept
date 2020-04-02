07bab87a03838870e403a30ed2dfc89b7ae48e31
gurkay <g_basyigit@hotmail.com>
2020-04-03 00:35:12 +0300

    hw4

--- src/com/homework4/HTMLBuilderTest.java
+++ src/com/homework4/HTMLBuilderTest.java
@@ -6,18 +6,19 @@ import java.util.*;
 public class HTMLBuilderTest {
 
 	public static void main(String[] args) throws Exception {
-		
-		new HtmlBuilder().
-			italic().addText("CENG 431").lineBreak().unitalic().
+
+		new HtmlBuilder().italic().addText("CENG 431");
+/*		.lineBreak().unitalic(). 
 			bold().addText("Design Pattern ").unbold().
 			underline().addText("course is easy ;)").ununderline().paragraph().
-			addHyperlink("https://www.coursera.org/learn/design-patterns", "MOOC Course link").lineBreak().
-			addImage("Java.png","Java").
+			addHyperlink("https://www.coursera.org/learn/design-patterns",
+					"MOOC Course link").lineBreak(). addImage("Java.png","Java").
 			addOrderedList(new Object[] {"Java", "is", "number", 1, "language :)"}).
-			addUnorderedList(new Object[] {"But", "I", "am", 100.0, "sure", "that", "C++", "is", "still", "widely", "used"}).
-			addTable(new Object[][] {{1, 5, "aha"}, {true, 0, 9}, {null, 19L, "test"}, {'C', "++", 5.0}}).
-			build().
-			exportToFile("builderTest.html");
+			addUnorderedList(new Object[] {"But", "I", "am", 100.0, "sure", "that",
+					"C++", "is", "still", "widely", "used"}). addTable(new Object[][] {{1, 5,
+					"aha"}, {true, 0, 9}, {null, 19L, "test"}, {'C', "++", 5.0}}). 
+			build().exportToFile("builderTest.html");*/
+		 
 	}
 
 }
@@ -26,19 +27,33 @@ class HtmlBuilder {
 	/****************************************************************
 	 * 14050162001 Gurkay BASYIGIT
 	 ****************************************************************/
-	private final String beginHTML = "<!DOCTYPE html><html><body>";
-	private final String endHTML = "</body></html>";
-	
-	public HtmlBuilder() {
-		// TODO Auto-generated constructor stub
+	static Queue<String> queue = new LinkedList<String>();
+	static private HtmlBuilder htmlBuilder = null;
+
+	HtmlBuilder() {
+
+		if (htmlBuilder == null) {
+
+			synchronized (queue) {
+				queue.add("");
+			}
+		}
 		
 	}
-
-	public Object italic() {
-		// TODO Auto-generated method stub
-		return null;
+	
+	public HtmlBuilder addText(String str) {
+		synchronized (queue) {
+			queue.add(str);
+		}
+		return htmlBuilder;
 	}
 	
-	
-	
+	public String italic() {
+		synchronized (queue) {
+			queue.add("<I>");
+		}
+		return "";
+	}
+
+
 }
