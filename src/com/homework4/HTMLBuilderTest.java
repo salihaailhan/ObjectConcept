07be9b603705cdfe9eb2635b0f11541bae346694
gurkay <g_basyigit@hotmail.com>
2020-04-04 19:35:22 +0300

    homework4

--- src/com/homework4/HTMLBuilderTest.java
+++ src/com/homework4/HTMLBuilderTest.java
@@ -1,14 +1,12 @@
 package com.homework4;
 
 import java.io.*;
-import java.util.*;
 
 public class HTMLBuilderTest {
 
 	public static void main(String[] args) throws Exception {
 
-		new HtmlBuilder().italic().addText("CENG 431");
-/*		.lineBreak().unitalic(). 
+		new HtmlBuilder().italic().addText("CENG 431").lineBreak().unitalic(). 
 			bold().addText("Design Pattern ").unbold().
 			underline().addText("course is easy ;)").ununderline().paragraph().
 			addHyperlink("https://www.coursera.org/learn/design-patterns",
@@ -17,7 +15,7 @@ public class HTMLBuilderTest {
 			addUnorderedList(new Object[] {"But", "I", "am", 100.0, "sure", "that",
 					"C++", "is", "still", "widely", "used"}). addTable(new Object[][] {{1, 5,
 					"aha"}, {true, 0, 9}, {null, 19L, "test"}, {'C', "++", 5.0}}). 
-			build().exportToFile("builderTest.html");*/
+			build().exportToFile("builderTest.html");
 		 
 	}
 
@@ -26,34 +24,120 @@ public class HTMLBuilderTest {
 class HtmlBuilder {
 	/****************************************************************
 	 * 14050162001 Gurkay BASYIGIT
+	 * source : https://dzone.com/articles/design-patterns-the-builder-pattern
 	 ****************************************************************/
-	static Queue<String> queue = new LinkedList<String>();
-	static private HtmlBuilder htmlBuilder = null;
+	
+	private final String BEGIN = "<HTML><BODY>";
+	private String text = BEGIN;
+	private final String END = "</BODY></HTML>";
 
-	HtmlBuilder() {
-
-		if (htmlBuilder == null) {
-
-			synchronized (queue) {
-				queue.add("");
-			}
-		}
+	public HtmlBuilder exportToFile(String fileName) {
 		
+		FileWriter myWrite;
+		
+		try {
+			myWrite = new FileWriter(fileName);
+			myWrite.write(this.text);
+			myWrite.close();
+			System.out.println("File seccessfully wrote to the html file");
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+						
+		return this;
 	}
 	
-	public HtmlBuilder addText(String str) {
-		synchronized (queue) {
-			queue.add(str);
-		}
+	public HtmlBuilder build() {
+		this.text += END;
+		HtmlBuilder htmlBuilder = new HtmlBuilder();
+		htmlBuilder.text = this.text;					
 		return htmlBuilder;
 	}
 	
-	public String italic() {
-		synchronized (queue) {
-			queue.add("<I>");
+	public HtmlBuilder addTable(Object[][] list) {
+		this.text += "<TABLE>";
+		for(int i=0; i < list.length; i++) {
+			this.text += "<TR>";
+			for(int j=0; i < list[i].length; i++) {
+				this.text += "<TD>" + list[i][j] + "</TD>";	
+			}
+			this.text += "</TR";
 		}
-		return "";
+		this.text += "</TABLE>";
+		return this;
 	}
 
-
+	public HtmlBuilder addOrderedList(Object[] list) {
+		this.text += "<OL>";
+		for(int i=0; i < list.length; i++) {
+			this.text += "<LI>" + list[i];
+		}
+		this.text += "</OL>";
+		return this;
+	}
+	
+	public HtmlBuilder addUnorderedList(Object[] list) {
+		this.text += "<OL type=\"disc\">";
+		for(int i=0; i < list.length; i++) {
+			this.text += "<LI>" + list[i];
+		}
+		this.text += "</OL>";
+		return this;
+	}
+	
+	public HtmlBuilder addText(String str) {
+		this.text += str;
+		return this;
+	}	
+	
+	public HtmlBuilder addHyperlink(String lnk, String str) {
+		this.text += "<A HREF=\"" + lnk + "\">" + str + "</A>";
+		return this;
+	}
+	
+	public HtmlBuilder addImage(String src, String str) {
+		this.text += "<IMG SRC=\"" + src + "\" ALT=\"" + str + "\">";
+		return this;
+	}	
+	
+	public HtmlBuilder paragraph() {
+		this.text += "<P>";
+		return this;
+	}
+	
+	public HtmlBuilder underline() {
+		this.text += "<U>";
+		return this;
+	}
+	
+	public HtmlBuilder ununderline() {
+		this.text += "</U>";
+		return this;
+	}
+	
+	public HtmlBuilder italic() {
+		this.text += "<I>";
+		return this;
+	}
+	
+	public HtmlBuilder unitalic() {
+		this.text += "</I>";
+		return this;
+	}
+	
+	public HtmlBuilder bold() {
+		this.text += "<B>";
+		return this;
+	}
+	
+	public HtmlBuilder unbold() {
+		this.text += "</B>";
+		return this;
+	}
+	
+	public HtmlBuilder lineBreak() {
+		this.text += "<BR>";
+		return this;
+	}
 }
